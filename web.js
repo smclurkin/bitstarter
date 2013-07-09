132fa5f66834ea2c7209d267a9597d1e41dac368
Steven McLurkin <smclurkin@yahoo.com>
2013-07-09 04:27:57 +0000

    Reading index.html

--- web.js
+++ web.js
@@ -2,8 +2,11 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var buff = fs.readFile('./index.html');
+var message = buff.toString();
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(message);
 });
 
 var port = process.env.PORT || 5000;
